f9591e6816bfc9895d4a2070ec265495eec932ce
woodsy-sounding-wilful-sapwood <39788096+woodsy-sounding-wilful-sapwood@users.noreply.github.com>
2018-06-11 12:36:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['tensorflow>=1.8.0', 'scipy>=1.1.0', 'sh>=1.12.14', 'numpy>=1.14.3']
+REQUIRED_PACKAGES = ['tensorflow>=1.8.0', 'scipy>=1.1.0', 'sh>=1.12.14', 'numpy>=1.14.3', 'sklearn>=0.19.1', 'pandas>=0.22.0']
 
 setup(
 	name = 'trainer',
-	version = '0.1',
+	version = '0.2',
 	install_requires = REQUIRED_PACKAGES,
 	packages = find_packages(),
 	include_package_data = True,
-	description = 'Netflix with WALS'
+	description = 'Recommender System'
 )
