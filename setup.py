001ad38152f80eb3321c95516e3f46238f205f9f
woodsy-sounding-wilful-sapwood <39788096+woodsy-sounding-wilful-sapwood@users.noreply.github.com>
2018-07-05 17:58:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['tensorflow>=1.8.0', 'scipy>=1.1.0', 'sh>=1.12.14', 'numpy>=1.14.3', 'scikit-learn>=0.19.1', 'pandas>=0.22.0', 'flask>=1.0.2', 'redis>=2.10.5']
+REQUIRED_PACKAGES = ['tensorflow>=1.8.0', 'scipy>=1.1.0', 'sh>=1.12.14', 'numpy>=1.14.3', 'scikit-learn>=0.19.1', 'pandas>=0.22.0', 'flask>=1.0.2', 'redis>=2.10.5', 'matplotlib>=2.2.2']
 
 setup(
 	name = 'trainer',
